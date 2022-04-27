b7b91327f4f4ca3e51d6ede894af93f096eff2d7
Prabhjot Singh Grewal <psg918@ic.ac.uk>
2022-04-27 02:54:32 +0100

    Add visualisation function for agents actions in environment

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+import numpy as np
+
 from agents.agent import RandomAgent
 from envs.env import BasicEnv, MiniGridEnvWrapper
 from envs.gym_minigrid.register import register
@@ -8,6 +10,7 @@ AGENT_CHOICES = ["random"]
 ENV_CHOICES = ["basic", "unsafe-small", "unsafe-med"]
 MAX_EPISODE_LENGTH = 100
 MAX_TIME_STEPS = 100
+VISUALISATION_EPISODES = 5
 
 def init_env(args):
   if args.env == "basic":
@@ -54,6 +57,34 @@ def run_agent(env, agent, args):
   for s, a, r, new_s in trace:
     print("State:", s, "Action:", a, "Reward:", r, "New State:", new_s)
 
+def visualise_agent(env, agent, args):
+  """Run agent in environment and visualise agent's path."""
+  if args.gif:
+    from array2gif import write_gif
+    frames = []
+
+  env._env.render('human')
+
+  for _ in range(args.vis_eps):
+    obs = env.reset()
+
+    while True:
+      env._env.render('human')
+      if args.gif:
+        frames.append(np.moveaxis(env._env.render("rgb_array"), 2, 0))
+
+      action = agent.choose_action(obs)
+      obs, _, done, _ = env.step(action)
+      if done or env._env.window.closed:
+        break
+
+    if env._env.window.closed:
+        break
+
+  if args.gif:
+    print("Saving gif... ", end="")
+    write_gif(np.array(frames), args.gif+".gif", fps=1/0.1)
+    print("Done.")
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Agent and Environment options")
@@ -61,6 +92,8 @@ if __name__ == "__main__":
   parser.add_argument("--agent", default="random", choices=AGENT_CHOICES, help="Agent Type")
   parser.add_argument("--disable-cuda", action="store_false", help="Disable CUDA")
   parser.add_argument("--max-episode-length", type=int, default=MAX_EPISODE_LENGTH, help="Maximum number of steps per episode")
+  parser.add_argument("--gif", type=str, help="Filename for visualisation episodes gif")
+  parser.add_argument("--vis-eps", type=int, default=VISUALISATION_EPISODES, help="Number of episodes to visualise")
 
   # TODO: Figure out how to correctly register environments
   register(
@@ -77,3 +110,4 @@ if __name__ == "__main__":
   env, agent = initialise(args)
   train_agent(env, agent, args)
   run_agent(env, agent, args)
+  visualise_agent(env, agent, args)
