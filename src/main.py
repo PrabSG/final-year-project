d75a10e35aa5c1316357df44cf694ce210600c96
Prabhjot Singh Grewal <psg918@ic.ac.uk>
2022-05-14 17:42:57 +0100

    Simplify cmd line args to put all results in results_dir path

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 
-from array2gif import write_gif
 import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -11,7 +10,7 @@ from agents.agent import RandomAgent
 from agents.ddqn import DDQNAgent, DDQNParams
 from agents.ls_dreamer import LSDreamerParams, LatentShieldedDreamer
 from envs.env import init_env, BasicEnv, MiniGridEnvWrapper
-from utils import plot_training
+from utils import plot_training, visualise_agent
 
 AGENT_CHOICES = ["random", "ddqn", "ls-dreamer"]
 ENV_CHOICES = ["basic", "unsafe-micro", "unsafe-small", "unsafe-med"]
@@ -19,6 +18,7 @@ MAX_EPISODE_LENGTH = 50
 NUM_TRAINING_EPISODES = 100
 NUM_TESTING_EPISODES = 10
 VISUALISATION_EPISODES = 5
+VISUALISATION_FREQUENCY = 25
 
 ddqn_params = (1000, 256, [32, 32, 64], 0.001, 100, 0.9, 0.9, 0.05, 25)
 
@@ -42,14 +42,6 @@ def initialise(args):
 
 def train_agent(env, agent, args):
   return agent.train(env, writer=args.writer)
-    
-def visualise_agent(env, agent, args):
-  """Run agent in environment and visualise agent's path."""
-  _, frames = agent.run_tests(args.vis_eps, env, args, visualise=True)
-
-  print("Saving gif... ", end="")
-  write_gif(np.array(frames), args.gif+".gif", fps=1/0.1)
-  print("Done.")
   
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Agent and Environment options")
@@ -63,9 +55,8 @@ if __name__ == "__main__":
   # Script options
   parser.add_argument("--id", type=str, help="ID for results of run")
   parser.add_argument("--test-episodes", type=int, default=NUM_TESTING_EPISODES, help="Number of episodes to test the agent")
-  parser.add_argument("--plot", type=str, help="Filename for training losses plot")
-  parser.add_argument("--gif", type=str, help="Filename for visualisation episodes gif")
-  parser.add_argument("--vis-eps", type=int, default=VISUALISATION_EPISODES, help="Number of episodes to visualise")
+  parser.add_argument("--vis-eps", type=int, default=VISUALISATION_EPISODES, help="Number of episodes to visualise at each visualisation checkpoint")
+  parser.add_argument("--vis-freq", type=int, default=VISUALISATION_FREQUENCY, help="Number of episodes between ")
 
   args = parser.parse_args()
 
@@ -86,7 +77,6 @@ if __name__ == "__main__":
   env, agent = initialise(args)
   train_agent(env, agent, args)
   # n_episodes, episode_rs, n_steps, train_losses = train_agent(env, agent, args)
-  # plot_training(n_episodes, episode_rs, n_steps, train_losses, args.plot)
+  # plot_training(n_episodes, episode_rs, n_steps, train_losses, results_dir + "/train_plots")
   agent.run_tests(args.test_episodes, env, args, print_logging=True)
-  if args.gif != "":
-    visualise_agent(env, agent, args)
+  visualise_agent(env, agent, args)
