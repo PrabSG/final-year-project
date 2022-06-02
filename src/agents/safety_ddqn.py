f2c79f54a83c9cdc459759f60d7a0332d88fd021
Prabhjot Singh Grewal <psg918@ic.ac.uk>
2022-06-03 00:40:37 +0100

    Move spec encoder to own module, handle varying sequence length specs with PackedSequences

--- src/agents/safety_ddqn.py
+++ src/agents/safety_ddqn.py
@@ -1,10 +1,12 @@
 from collections import namedtuple
 import itertools
 import random
+from typing import Union
 
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence, PackedSequence
 
 from agents.agent import Agent
 from agents.ddqn import DDQNParams, Basic2dEncoder, QModel
@@ -18,10 +20,21 @@ SafetyTransition = namedtuple("SafetyTransition", ("state", "safety_spec", "acti
 SafetyState = namedtuple("SafetyState", ["env_state", "formula"])
 
 class SafetyDDQNParams(DDQNParams):
-  def __init__(self, spec_encoding_hidden_size=64, **kwargs):
-    super().__init__(**kwargs)
+  def __init__(self, *args, spec_encoding_hidden_size=64, **kwargs):
+    super().__init__(*args, **kwargs)
     self.spec_hidden_size = spec_encoding_hidden_size
 
+
+class SpecEncoder(nn.Module):
+  def __init__(self, encoding_size, hidden_size, num_layers=1) -> None:
+    super().__init__()
+    self.lstm = nn.LSTM(input_size=encoding_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
+
+  def forward(self, encoded_seq: Union[torch.Tensor, PackedSequence]) -> torch.Tensor:
+    hiddens, _ = self.lstm(encoded_seq)
+    if isinstance(hiddens, PackedSequence):
+      hiddens, _ = pad_packed_sequence(hiddens)
+    return hiddens.sum(dim=0)
 
 class SafetyDDQNAgent(Agent):
   def __init__(self, state_size, action_size, spec_encoding_size, params: SafetyDDQNParams) -> None:
@@ -51,10 +64,10 @@ class SafetyDDQNAgent(Agent):
       input_shape = state_size
 
     # Initialise safety specification encoder
-    self._spec_encoder = nn.LSTM(input_size=spec_encoding_size, hidden_size=params.spec_hidden_size, num_layers=1, batch_first=True, bidirectional=True)
+    self._spec_encoder = SpecEncoder(spec_encoding_size, params.spec_hidden_size).to(device=params.device)
 
-    self._policy_net = QModel(input_shape + (2 * params.spec_hidden_size), self.action_size, params.nn_sizes).to(self.params.device)
-    self._target_net = QModel(input_shape + (2 * params.spec_hidden_size), self.action_size, params.nn_sizes).to(self.params.device)
+    self._policy_net = QModel(input_shape + (2 * params.spec_hidden_size), self.action_size, params.nn_sizes).to(params.device)
+    self._target_net = QModel(input_shape + (2 * params.spec_hidden_size), self.action_size, params.nn_sizes).to(params.device)
     self._target_net.load_state_dict(self._policy_net.state_dict())
     
     if self.multi_dim_input:
@@ -101,7 +114,7 @@ class SafetyDDQNAgent(Agent):
       self._target_net_encoder.load_state_dict(self._policy_net_encoder.state_dict())
 
   def _policy_net_pass(self, state: SafetyState):
-    encoded_formula = torch.sum(self._spec_encoder(state.formula), dim=1)
+    encoded_formula = self._spec_encoder(state.formula)
 
     if self.multi_dim_input:
       encoded_state = self._policy_net_encoder(state.env_state)
@@ -110,7 +123,7 @@ class SafetyDDQNAgent(Agent):
       return self._policy_net(torch.cat((state.env_state, encoded_formula), dim=1))
 
   def _target_net_pass(self, state: SafetyState):
-    encoded_formula = torch.sum(self._spec_encoder(state.formula), dim=1)
+    encoded_formula = self._spec_encoder(state.formula)
 
     if self.multi_dim_input:
       encoded_state = self._target_net_encoder(state.env_state)
@@ -126,20 +139,22 @@ class SafetyDDQNAgent(Agent):
     batches = SafetyTransition(*zip(*transitions))
 
     state_batch = torch.cat(batches.state)
-    spec_batch = torch.cat(batches.safety_spec)
+    spec_batch = nn.utils.rnn.pack_sequence(batches.safety_spec, enforce_sorted=False)
     action_batch = torch.cat(batches.action)
     reward_batch = torch.cat(batches.reward)
     next_state_batch = torch.cat(batches.next_state)
-    prog_spec_batch = torch.cat(batches.prog_spec)
     violation = torch.tensor(batches.violation, dtype=torch.bool, device=self.params.device)
-    non_terminal_mask = np.logical_not(np.array(batches.done))
+    non_terminal_mask = torch.logical_not(torch.tensor(batches.done))
 
-    if np.sum(non_terminal_mask) > 0:
-      non_terminal_next_states = torch.cat([next_state_batch[i] for i in range(self.params.batch_size) if non_terminal_mask[i]])
-      non_terminal_prog_specs = torch.cat([prog_spec_batch[i] for i in range(self.params.batch_size) if non_terminal_mask[i]])
+    if torch.sum(non_terminal_mask) > 0:
+      non_terminal_next_states = next_state_batch[torch.nonzero(non_terminal_mask, as_tuple=True)]
+      # non_terminal_next_states = torch.tensor([next_state_batch[i] for i in range(self.params.batch_size) if non_terminal_mask[i]])
+      non_terminal_prog_specs = nn.utils.rnn.pack_sequence(
+        [batches.prog_spec[i] for i in range(self.params.batch_size) if non_terminal_mask[i]],
+        enforce_sorted=False)
     else:
-      non_terminal_next_states = torch.empty(0, self.state_size)
-      non_terminal_prog_specs = torch.empty(0, 0, self.spec_encoding_size)
+      non_terminal_next_states = torch.empty(0, self.state_size, device=self.params.device)
+      non_terminal_prog_specs = nn.utils.rnn.pack_sequence([torch.empty(0, self.spec_encoding_size, device=self.params.device)])
 
     safety_state_batch = SafetyState(state_batch, spec_batch)
     state_qs = self._policy_net_pass(safety_state_batch).gather(1, torch.argmax(action_batch, dim=1, keepdim=True))
@@ -147,10 +162,10 @@ class SafetyDDQNAgent(Agent):
     non_terminal_next_batch = SafetyState(non_terminal_next_states, non_terminal_prog_specs)
     next_state_vals = torch.zeros(self.params.batch_size, device=self.params.device)
     with torch.no_grad():
-      if np.sum(non_terminal_mask) > 0:
+      if torch.sum(non_terminal_mask) > 0:
         argmax_q_idx = self._policy_net_pass(non_terminal_next_batch).argmax(1).detach()
         q_vals = self._target_net_pass(non_terminal_next_batch).detach()
-        next_state_vals[non_terminal_mask] = q_vals[range(q_vals.shape[0]), argmax_q_idx]
+        next_state_vals[torch.nonzero(non_terminal_mask, as_tuple=True)] = q_vals[range(q_vals.shape[0]), argmax_q_idx]
     
     expected_qs = (next_state_vals * self.params.gamma) + reward_batch
 
@@ -183,13 +198,13 @@ class SafetyDDQNAgent(Agent):
         print(f"Agent exploring in episode {i_episode + 1}")
       env.reset(random_start=True)
 
-      safety_spec = env.get_safety_spec()
-      safety_spec_strs = safety_spec_to_str(safety_spec)
-      one_hot_safety_spec = get_one_hot_spec(safety_spec_strs, env.get_num_props()).unsqueeze(0).to(device=self.params.device)
+      raw_safety_spec = env.get_safety_spec()
+      safety_spec_strs = safety_spec_to_str(raw_safety_spec)
+      safety_spec = get_one_hot_spec(safety_spec_strs, env.get_num_props()).to(device=self.params.device)
 
       total_reward = 0
       state = env.get_observation().unsqueeze(0).to(self.params.device)
-      comb_state = SafetyState(state, one_hot_safety_spec)
+      comb_state = SafetyState(state, safety_spec.unsqueeze(1))
 
       self.metrics["train_losses"].append(0)
 
@@ -202,7 +217,7 @@ class SafetyDDQNAgent(Agent):
         next_state = next_state.unsqueeze(0).to(self.params.device)
         reward = torch.tensor([reward], device=self.params.device, dtype=torch.float)
         violation = info["violation"]
-        prog_safety_spec = get_one_hot_spec(safety_spec_to_str(info["prog_formula"]), env.get_num_props()).unsqueeze(0).to(device=self.params.device)
+        prog_safety_spec = get_one_hot_spec(safety_spec_to_str(info["prog_formula"]), env.get_num_props()).to(device=self.params.device)
 
         self._exp_replay.add(SafetyTransition(state, safety_spec, action, reward, next_state, prog_safety_spec, violation, done))
 
@@ -218,6 +233,7 @@ class SafetyDDQNAgent(Agent):
           break
         state = next_state
         safety_spec = prog_safety_spec
+        comb_state = SafetyState(state, safety_spec.unsqueeze(1))
 
       self.metrics["steps"].append(t+1 + (0 if len(self.metrics["steps"]) == 0 else self.metrics["steps"][-1]))
       self.metrics["episode_rewards"].append(total_reward)
